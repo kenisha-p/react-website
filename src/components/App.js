842022f3f59fb161b4d092096fa62b3099a09679
Kenisha <k4perera@uwaterloo.ca>
2021-12-04 19:29:15 -0500

    finish react app

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import Navbar from "./pages/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage/Home";
+import Services from "./pages/Services/Services";
+import Products from "./pages/Products/Products";
+import SignUp from "./pages/SignUp/SignUp";
+import Footer from "./pages/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
